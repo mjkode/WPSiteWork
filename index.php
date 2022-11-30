e1768b6824ffaee63744ce29219c4339deeb34ce
mjkode <49616828+mjkode@users.noreply.github.com>
2022-11-30 10:45:05 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
+	<link rel="icon" href="image/cropped-wpsiteworklogo.png">
+	<title>WPSITEWORK</title>
+</head>
+
+<body>
+	<!-- Header -->
+	<section id="header">
+		<div class="header container">
+			<div class="nav-bar">
+				<div class="brand">
+					<a href="#hero">
+						<h1><span>WP</span>Sitework</h1>
+					</a>
+				</div>
+				<div class="nav-list">
+					<div class="hamburger">
+					<div class="bar"></div>
+					</div>
+					<ul>
+						<li><a href="#hero" data-after="Home">Home</a></li>
+						<li><a href="#services" data-after="Service">Services</a></li>
+						<!-- <li><a href="#projects" data-after="Projects">Projects</a></li> -->
+						<li><a href="#about" data-after="About">About</a></li>
+						<li><a href="#contact" data-after="Contact">Contact</a></li>
+						<li><a href="https://www.wpsitework.com" data-after="Wordpress">Wordpress?</a></li>
+						<li><a href="javascript:window.open('https://www.wpsitework.com/JS_Location/', 'Find Me', 'width=400,height=600');">JavaScript?</a><li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- End Header -->
+
+	<!-- Hero Section  -->
+	<section id="hero">
+		<div class="hero container">
+			<!-- <div ="width: 42%" align= "right";> -->
+			<div>
+				<h1>WPSiteWork<span></span></h1>
+				<h1>Clean, Simple Sites<span></span></h1>
+				<h1>Built With HTML5 and JS<span></span></h1>
+				<a href="#contact" type="button" class="cta">Get Started Now</a>
+			</div>
+		</div>
+	</section>
+	<!-- End Hero Section  -->
+
+	<!-- Service Section -->
+	<section id="services">
+		<div class="services container">
+			<div class="services-top">
+				<h1 class="section-title"><span>S</span>ervices</h1>
+				<h2 class="section-title-sub">What I Can Do For <span>You</span></h2>
+			</div>
+			<div class="services-bottom">
+				<div class="service-item">
+					<div class="icon"><img src="./image/design.png"></div>
+					<h2>Design</h2>
+					<p>By creating a site with the look and feel of your brand, we can extend your reach to new customers</p>
+				</div>
+				<div class="service-item">
+					<div class="icon"><img src="./image/develope.png"></div>
+					<h2>Develope</h2>
+					<p>Building your site with HTML, CSS, and plain vanilla Javascript, your site's code will be as simple as possible</p>
+				</div>
+				<div class="service-item">
+					<div class="icon"><img src="./image/deploy.png"></div>
+					<h2>Deploy</h2>
+					<p>A 'responsive' site retaina it's look and feel across multiple devices, assuring access to potential customers</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- End Service Section -->
+
+	<!-- About Section -->
+	<section id="about">
+		<div class="about container">
+			<div class="about-top">
+				<h1 class="section-title">About <span>me</span></h1>
+				<h2 class="section-title-sub">This Is A<span> One Techie </span>Shop</h2>
+				<div class="about-img">
+					<img src="./image/wpsiteworkmascottransparent.png" alt="img">
+				</div>
+				<h2>I build one page, multisectional web sites for entreprenures, non-profits, and small businesses.
+				</h2>
+			</div>
+		</div>
+	</section>
+	<!-- End About Section -->
+
+	<!-- Contact Section -->
+	<section id="contact">
+		<div class="contact container">
+			<div>		
+				<h1 class="section-title">Contact <span>info</span></h1>
+			</div>	
+			<div class="contact-items">
+				<div class="contact-item">
+					<div class="icon"><img src="./image/phone.png" alt="img"></div>
+					<div class="contact-info">
+						<h1>Phone</h1>
+						<h2>+1 434 227 3305</h2>
+					</div>	
+				</div>
+				<div class="contact-item">
+					<div class="icon"><img src="./image/email.png" alt="img"></div>
+					<div class="contact-info">
+						<h1>Email</h1>
+						<h2>mjk@wpsitework.com</h2>
+					</div>
+				</div>
+				<div class="contact-item">
+					<div class="icon"><img src="./image/address.png" alt="img"></div>
+					<div class="contact-info">
+						<h1>Address</h1>
+						<h2>Charlottesvillie, Virginia</h2>
+					</div>
+				</div>
+				</div>
+			</div>
+		</section>
+	<!-- End Contact Section -->
+
+	<!-- Footer -->
+	<section id="footer">
+		<div class="footer container">
+			<div class="brand">
+				<h1><span>M</span>ichael<span>K</span>iger</h1>
+			</div>
+			<h2>Have a bias towards action</h2>
+			<div class="social-icon">
+				<div class="social-item">
+					<a href="#">
+						<a href="https://www.linkedin.com/in/michael-kiger/">
+							<img src="https://img.icons8.com/ios-filled/344/facebook-new.png" />
+						</a>
+					</a>
+				</div>
+				<div class="social-item">
+					<a href="#">
+						<a href="https://www.twitter.com/wpsitework/">
+							<img src="https://img.icons8.com/ios-filled/344/twitter.png" />
+						</a>
+					</a>
+				</div>
+			</div>
+			<p>Copyright © 2020 Kiger. All rights reserved</p>
+		</div>
+	</section>
+	<!-- End Footer -->
+	<script src="./app.js"></script>
+</body>
+
+</html>
